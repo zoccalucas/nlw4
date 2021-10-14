3422402f2ebef26641c17b1d9b6a473efd296e6a
Lucas Zocca <lucaszoccasoares@gmail.com>
2021-10-14 12:37:13 -0300

    refact: Modificando o método de testes do banco de dados

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -1,12 +1,18 @@
-import request from "supertest";
 import { app } from "../api/app";
-
 import createConnection from "../core/database";
+import { getConnection } from "typeorm";
+import request from "supertest";
 
 describe("Surveys", () => {
   beforeAll(async () => {
     const connection = await createConnection();
     await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    const connection = getConnection();
+    await connection.dropDatabase();
+    await connection.close();
   });
 
   it("Should be able to create a new survey", async () => {
--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,13 +1,20 @@
-import request from "supertest";
 import { app } from "../api/app";
-
 import createConnection from "../core/database";
+import { getConnection } from "typeorm";
+import request from "supertest";
 
 describe("Users", () => {
   beforeAll(async () => {
     const connection = await createConnection();
     await connection.runMigrations();
   });
+
+  afterAll(async () => {
+    const connection = getConnection();
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
 
   it("Should be able to create a new user", async () => {
     const response = await request(app).post("/users").send({
