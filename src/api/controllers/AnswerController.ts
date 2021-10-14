88c507ba3fe51a71fadcbdaea9d445c743581eb6
Lucas Zocca <lucaszoccasoares@gmail.com>
2021-10-13 23:48:45 -0300

    feat: Adiciona controller para respostas do usuário

---
+++ src/api/controllers/AnswerController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+
+import { SurveysUsersRepository } from "../../core/repositories/SurveysUsersRepository";
+import { getCustomRepository } from "typeorm";
+
+class AnswerController {
+  async execute(req: Request, res: Response) {
+    const { value } = req.params;
+    const { u } = req.query;
+
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+    const surveyUser = await surveysUsersRepository.findOne({
+      id: String(u),
+    });
+
+    if (!surveyUser) {
+      return res.status(400).json({
+        error: "Survey User does not exists!",
+      });
+    }
+
+    surveyUser.value = Number(value);
+
+    await surveysUsersRepository.save(surveyUser);
+
+    return res.json(surveyUser);
+  }
+}
+
+export default new AnswerController();
