7db2b609a169c319b9f141e83c58a4182f02d84c
Lucas Zocca <lucaszoccasoares@gmail.com>
2021-10-13 22:39:30 -0300

    feat: Atualizando a porta padrão da API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
-import { NODE_PORT } from "../config";
+import { API_PORT } from "../config";
 import { app } from "./app";
 
-app.listen(NODE_PORT, () => {
-  console.log(`Server running at http://localhost:${NODE_PORT} 🚀`);
+app.listen(API_PORT, () => {
+  console.log(`Server running at http://localhost:${API_PORT} 🚀`);
 });
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const NODE_PORT = process.env.NODE_PORT || 3333;
+export const API_PORT = 3333;
